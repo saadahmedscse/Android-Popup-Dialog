717907ff2d15297bd35fef311f37348d910bd033
Saad <saadahmedscse@gmail.com>
2022-10-16 16:34:10 +0600

    Main class functionalities

--- popupDialog/src/main/java/com/saadahmedsoft/popupdialog/PopupDialog.java
+++ popupDialog/src/main/java/com/saadahmedsoft/popupdialog/PopupDialog.java
@@ -1,5 +1,29 @@
 package com.saadahmedsoft.popupdialog;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+
 public class PopupDialog {
 
+    private final Dialog dialog;
+    private final Context context;
+    @SuppressLint("StaticFieldLeak")
+    private static PopupDialog instance = null;
+
+    private PopupDialog(Context context) {
+        this.context = context;
+        dialog = new Dialog(context);
+    }
+
+    public static PopupDialog getInstance(Context context) {
+        if (instance == null) {
+            instance = new PopupDialog(context);
+        }
+        return instance;
+    }
+
+    public CreateDialog setStyle(Styles style) {
+        return CreateDialog.getInstance(context, style, dialog);
+    }
 }
