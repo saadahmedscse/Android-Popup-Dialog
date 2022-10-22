a2fdf39fe6f74f71bf6b98a4e9347a8f12bfb79d
Saad <saadahmedscse@gmail.com>
2022-10-22 15:03:10 +0600

    Removed testing code

--- app/src/main/java/com/caffeine/popupdialog/MainActivity.java
+++ app/src/main/java/com/caffeine/popupdialog/MainActivity.java
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.os.Bundle;
 
-import com.saadahmedsoft.popupdialog.PopupDialog;
-import com.saadahmedsoft.popupdialog.Styles;
-import com.saadahmedsoft.popupdialog.listener.OnDialogButtonClickListener;
+//import com.saadahmedsoft.popupdialog.PopupDialog;
+//import com.saadahmedsoft.popupdialog.Styles;
+//import com.saadahmedsoft.popupdialog.listener.OnDialogButtonClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,16 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PopupDialog.getInstance(this)
-                .setStyle(Styles.STANDARD)
-                .setHeading("This is heading")
-                .setDescription("This is the description")
-                .setDismissButtonText("Close")
-                .showDialog(new OnDialogButtonClickListener() {
-                    @Override
-                    public void onDismissClicked(Dialog dialog) {
-                        dialog.dismiss();
-                    }
-                });
+//        PopupDialog.getInstance(this)
+//                .setStyle(Styles.STANDARD)
+//                .setHeading("This is heading")
+//                .setDescription("This is the description")
+//                .setDismissButtonText("Close")
+//                .showDialog(new OnDialogButtonClickListener() {
+//                    @Override
+//                    public void onDismissClicked(Dialog dialog) {
+//                        dialog.dismiss();
+//                    }
+//                });
     }
 }
