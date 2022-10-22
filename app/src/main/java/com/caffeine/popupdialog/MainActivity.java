9c5ec5a02a9493a055b27e8a2a9cd4a1ba18009a
Saad <saadahmedscse@gmail.com>
2022-10-22 12:26:41 +0600

    Testing dialog

--- app/src/main/java/com/caffeine/popupdialog/MainActivity.java
+++ app/src/main/java/com/caffeine/popupdialog/MainActivity.java
@@ -2,7 +2,12 @@ package com.caffeine.popupdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+
+import com.saadahmedsoft.popupdialog.PopupDialog;
+import com.saadahmedsoft.popupdialog.Styles;
+import com.saadahmedsoft.popupdialog.listener.OnDialogButtonClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PopupDialog.getInstance(this)
+                .setStyle(Styles.STANDARD)
+                .setHeading("This is heading")
+                .setDescription("This is the description")
+                .setDismissButtonText("Close")
+                .showDialog(new OnDialogButtonClickListener() {
+                    @Override
+                    public void onDismissClicked(Dialog dialog) {
+                        dialog.dismiss();
+                    }
+                });
     }
 }
